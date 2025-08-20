5b5c6d3c234381c026f3413facd95ed6fb80fdd8
Nguyen-Thanh-Huy-io <nguyenthanhhuy021005@gmail.com>
2025-08-20 21:02:46 +0700

    Bài tập tuần 1

---
+++ src/main/java/vn/iotstar/profile/Profile.java
@@ -0,0 +1,52 @@
+package vn.iotstar.profile;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class Profile
+ */
+@WebServlet(urlPatterns= {"/profile"})
+public class Profile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Profile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+		HttpSession session = request.getSession(false);
+		PrintWriter out = response.getWriter();
+		if (session != null && session.getAttribute("username") != null) {
+            String name = (String) session.getAttribute("username");
+            out.print("Chào bạn, " + name + " đến với trang quản lý tài khoản");
+		}
+		else {
+			response.sendRedirect("/Helloworld/Login.html");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
